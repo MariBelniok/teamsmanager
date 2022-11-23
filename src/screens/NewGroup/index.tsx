e598b8facc7fe7d822946d99957da15742e28b4a
maribelniok <mariannabelniok@outlook.com>
2022-11-23 20:44:37 -0300

    ADD: new group screen

---
+++ src/screens/NewGroup/index.tsx
@@ -0,0 +1,21 @@
+import { Header } from "@components/Header";
+import { Highlight } from "@components/Highlight";
+import { Button } from "@components/Button";
+import { Container, Content, Icon } from "./styles";
+
+export function NewGroup() {
+  return (
+    <Container>
+      <Header showBackButton />
+
+      <Content>
+        <Icon />
+
+        <Highlight title='Nova turma' subTitle='Crie a turma para adicionar as pessoas' />
+  
+        <Button title='Criar' />
+      </Content>
+
+    </Container>
+  )
+}
