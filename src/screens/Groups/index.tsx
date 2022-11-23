ec48d9ac06bc58ab4fef94f94c8e271be28d78ac
maribelniok <mariannabelniok@outlook.com>
2022-11-23 19:41:07 -0300

    ADD: Header and hightlight components

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,13 @@
 import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
 import * as S from './styles';
 
 export function Groups() {
   return (
     <S.Container>
       <Header />
+
+      <Highlight title='Turmas' subTitle='Jogue com a sua turma' />
     </S.Container>
   );
 }
