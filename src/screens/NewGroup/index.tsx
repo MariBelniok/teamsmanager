00632be2e7e8085a0b51583c230f46d8068a39a0
maribelniok <mariannabelniok@outlook.com>
2022-11-26 15:35:08 -0300

    ADD: route param

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -4,12 +4,15 @@ import { Button } from "@components/Button";
 import { Input } from "@components/Input";
 import { Container, Content, Icon } from "./styles";
 import { useNavigation } from "@react-navigation/native";
+import { useState } from "react";
 
 export function NewGroup() {
+  const [group, setGroup] = useState('');
+
   const navigation = useNavigation();
 
   function handleNew() {
-    navigation.navigate('players', { group: 'Rocket' })
+    navigation.navigate('players', { group })
   }
 
   return (
@@ -23,6 +26,7 @@ export function NewGroup() {
 
         <Input 
           placeholder="Nome da turma"
+          onChangeText={setGroup}
         /> 
   
         <Button 
